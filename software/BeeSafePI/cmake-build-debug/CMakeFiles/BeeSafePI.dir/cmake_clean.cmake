57565b847ebea5351abbc33b5c08d7dbab502f94
dans-acc <danv123456789@gmail.com>
2020-02-18 16:28:03 +0000

    Initial BeeSafePI commit

---
+++ software/BeeSafePI/cmake-build-debug/CMakeFiles/BeeSafePI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BeeSafePI.dir/main.cpp.o"
+  "BeeSafePI.pdb"
+  "BeeSafePI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BeeSafePI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ software/BeeSafePI/cmake-build-debug/CMakeFiles/BeeSafePI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BeeSafePI.
+# This may be replaced when dependencies are built.
