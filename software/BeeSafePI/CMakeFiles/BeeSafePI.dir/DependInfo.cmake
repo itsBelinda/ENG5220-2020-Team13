fafe1a8742e84afffe3f7afcd424a8661975e76c
dans-acc <danv123456789@gmail.com>
2020-03-15 18:12:21 +0000

    Main has been braight back

---
+++ software/BeeSafePI/CMakeFiles/BeeSafePI.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/Main.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/Main.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/Account.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/Account.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/builders/AccountBuilder.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/builders/AccountBuilder.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/contact/Contact.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/contact/Contact.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/geo/Fence.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/geo/Fence.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/geo/PolyFence.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/geo/PolyFence.cpp.o"
+  "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/src/device/geo/RoundFence.cpp" "/Users/danielsvasiljevs/CLionProjects/BeeSafe/software/BeeSafePI/CMakeFiles/BeeSafePI.dir/src/device/geo/RoundFence.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPPREST_FORCE_HTTP_CLIENT_ASIO"
+  "CPPREST_FORCE_HTTP_LISTENER_ASIO"
+  "CPPREST_NO_SSL_LEAK_SUPPRESS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/opt/openssl/bin/openssl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ software/BeeSafePI/CMakeFiles/BeeSafePI.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "BeeSafePI"
+  "BeeSafePI.pdb"
+  "CMakeFiles/BeeSafePI.dir/src/Main.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/Account.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/builders/AccountBuilder.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/contact/Contact.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/geo/Fence.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/geo/PolyFence.cpp.o"
+  "CMakeFiles/BeeSafePI.dir/src/device/geo/RoundFence.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BeeSafePI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
