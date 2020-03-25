a61cf26e895a7167a5c95907cbccba97534ba36d
Belinda <belinda.kneubuehler@gmail.com>
2020-03-25 20:43:22 +0000

    added 3 step command to ublox (send cmd / get response / get status)

--- software/BeeSafePI/src/comms/UBlox.cpp
+++ software/BeeSafePI/src/comms/UBlox.cpp
@@ -1,13 +1,16 @@
+#include <boost/compatibility/cpp_c_headers/cstring>
 #include "UBlox.h"
 
+#define  MAX_CMD_LENGTH 544
 // Define the AT commands that are used on the U-Blox device.
 #define AT_COMMAND_GET_MODEL_NUMBER "ATI0\r\n"
 #define AT_COMMAND_GET_IMEI "AT+GSN1\r\n"
 #define AT_COMMAND_GET_LOCATION ""
 
-// Define the responses.
-#define AT_RESPONSE_OK "OK"
-#define AT_RESPONSE_ERROR "ERROR"
+// Define the states.
+#define AT_STATUS_OK "OK"
+#define AT_STATUS_ERROR "ERROR"
+#define AT_STATUS_ABORTED "ABORTED"
 
 // Define expected response sizes
 #define SZ_RESPONSE_IMEI 17 //TODO: check
@@ -89,7 +92,7 @@ int UBlox::getIMEI(std::string &imei)
         printf("unexpected echo cmd: sent %s, received: %s\n", AT_COMMAND_GET_IMEI, echoBuffer);
         return -1;
     } else {
-        printf("Command successfully echoed");
+        printf("Command successfully echoed\n");
     }
 
 
@@ -100,10 +103,10 @@ int UBlox::getIMEI(std::string &imei)
 
     //TODO: do in separate function
     if (nRx != SZ_RESPONSE_IMEI) {//TODO: check formatting of return first|| strcmp(AT_COMMAND_GET_IMEI,echoBuffer)) {
-        printf("unexpected answer received: %s\n",  imeiBuffer);
+        printf("unexpected answer received: %s\n", imeiBuffer);
         return -1;
     } else {
-        printf("Answer successfully received");
+        printf("Answer successfully received\n");
     }
 
     char statusBuffer[SZ_RESPONSE_STATUS] = {'\0'};
@@ -111,10 +114,10 @@ int UBlox::getIMEI(std::string &imei)
 
     //TODO: do in separate function
     if (nRx != SZ_RESPONSE_IMEI) {//TODO: check formatting of return first|| strcmp(AT_COMMAND_GET_IMEI,echoBuffer)) {
-        printf("unexpected answer received: %s\n",  statusBuffer);
+        printf("unexpected answer received: %s\n", statusBuffer);
         return -1;
     } else {
-        printf("Answer successfully received");
+        printf("Answer successfully received\n");
     }
 
 
@@ -122,7 +125,118 @@ int UBlox::getIMEI(std::string &imei)
     return -1;
 }
 
+int UBlox::getIMEI2(std::string &imei)
+{
+    bool result = false;
+    char replyBuffer[MAX_CMD_LENGTH];
+    printf("Writing command IMEI\n");
+
+    // Write the at command via uart.
+    result = sendCmd(AT_COMMAND_GET_IMEI);
+    if (result != true) {
+        return -1; // TODO: error codes? or handle here?
+    }
+
+    printf("Command written\n");
+
+    result = getReply(replyBuffer, SZ_RESPONSE_IMEI);
+    if (result != true) {
+        return -1; // TODO: error codes? or handle here?
+    }
+
+    printf("Response received\n");
+
+    result = checkStatus();
+    if (result != true) {
+        return -1; // TODO: error codes? or handle here?
+    }
+
+    imei = replyBuffer; // char array to string //todo: trailing zeros?
+    printf("Status ok received\n");
+
+    return 0;
+}
+
 int UBlox::getLocation(double &lat, double &lng)
 {
     return -1;
+}
+
+
+bool UBlox::sendCmd(const char *const cmdBuffer)
+{
+    int expSize = sizeof(cmdBuffer);
+    char echoBuffer[sizeof(cmdBuffer)] = {'\0'};
+
+    // Write the at command via uart.
+    ssize_t nTx = uart.writeBuffer(cmdBuffer);
+    if (nTx == -1) {
+        return false;
+    }
+
+    printf("Command written\n");
+    ssize_t nRx = uart.readBuffer(echoBuffer, expSize, RX_TIMEOUT);
+
+    if (nRx != nTx || strcmp(cmdBuffer, echoBuffer)) {
+        printf("unexpected echo cmd: sent %s, received: %s\n", AT_COMMAND_GET_IMEI, echoBuffer);
+        return false;
+    } else {
+        printf("Command was successfully echoed %s\n", echoBuffer);
+    }
+
+    return true;
+}
+
+// todo: possilby get rid of siez
+bool UBlox::getReply(char *const replyBuffer, int expSize) // TODO: what happens if buffer size too small? fixed
+// size?
+// static?
+{
+    ssize_t nRx = uart.readBuffer(replyBuffer, expSize, RX_TIMEOUT);
+    if (nRx != expSize) {//TODO:
+        printf("unexpected answer received: %s\n", replyBuffer);
+        return false;
+    } else {
+        printf("Answer successfully received\n");
+    }
+
+    return true;
+}
+
+bool UBlox::checkStatus()
+{
+    char statusBuffer[SZ_RESPONSE_STATUS] = {'\0'};
+    ssize_t nRx = uart.readBuffer(statusBuffer, SZ_RESPONSE_STATUS, RX_TIMEOUT);
+
+    if (nRx != SZ_RESPONSE_STATUS) {
+        printf("unexpected answer received: %s\n", statusBuffer);
+        return false;
+    } else {
+        std::string reply = statusBuffer;
+        if (findCharArray(AT_STATUS_OK, statusBuffer) == true) {
+            printf("Status OK\n");
+            return true;
+        } else if (findCharArray(AT_STATUS_ERROR, statusBuffer) == true) {
+            printf("Status ERROR\n"); // TODO: handle here or error code?
+            // TODO: error status needs to be handled
+            return false;
+        } else if (findCharArray(AT_STATUS_OK, statusBuffer) == true) {
+            // TODO: aborted status needs to be handled
+            printf("Status ABORTED\n"); // TODO: handle here or error code?
+            return false;
+        }
+
+    }
+}
+
+// todo helper
+bool UBlox::findCharArray(const char *const needle, const char *const haystack)
+{
+    const char *result = NULL;
+    //std::string::find()
+    result = std::strstr(needle, haystack);
+    if (result != NULL) {
+        return true;
+    }
+    return false;
 }
--- software/BeeSafePI/src/comms/UBlox.h
+++ software/BeeSafePI/src/comms/UBlox.h
@@ -25,12 +25,17 @@ public:
     // Methods for querying the U-Blox chip.
     int getModelNumber(std::string &modelNumber);
     int getIMEI(std::string &imei);
+    int getIMEI2(std::string &imei);
     int getLocation(double &lat, double &lng);
 
 private:
 
     // Configure the UBlox device.
     int conf();
+    bool sendCmd(const char * const cmdBuffer);
+    bool getReply(char *const replyBuffer, int expSize);
+    bool checkStatus();
+    bool findCharArray(const char *const needle, const char *const haystack);
 
 private:
 
