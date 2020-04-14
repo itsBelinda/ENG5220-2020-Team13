f6ae7d3109ca08743c707d092881f7c4fb1d55b2
dans-acc <danv123456789@gmail.com>
2020-04-14 23:26:54 +0100

    Testing AccountBuilder fix

--- software/BeeSafePI/src/device/AccountBuilder.cpp
+++ software/BeeSafePI/src/device/AccountBuilder.cpp
@@ -196,6 +196,7 @@ std::map<int, std::vector<std::pair<std::tm, std::tm>>> AccountBuilder::buildWee
     struct std::tm toTm = {0};
     for (int i = 0; i < days->length(); ++i) {
         if (!jsonWeek.has_object_field(U(days[i]))) {
+            std::cout << "Does not have day " << days[i] << std::endl;
             continue;
         }
         const web::json::array& times = jsonWeek.at(U(days[i])).as_array();
